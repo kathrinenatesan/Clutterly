f5879463f8406e1be1cee43572d119cc79e9f5fd
Kathy Natesan <kathy.natesan@gmail.com>
2026-07-09 11:38:09 -0400

    basic folder scanner - scans dirs, storage summary with categories by extension

---
+++ scanner.py
@@ -0,0 +1,179 @@
+import os
+from pathlib import Path
+from datetime import datetime
+import sys
+
+category = {
+    "Images": [
+        ".png",
+        ".jpg",
+        ".jpeg",
+        ".heic",
+        ".gif",
+        ".svg",
+        ".webp",
+        ".bmp",
+        ".tiff",
+        ".ico"
+    ],
+    "Documents": [
+        ".pdf",
+        ".docx",
+        ".txt",
+        ".rtf",
+        ".pptx",
+        ".xlsx",
+        ".csv",
+        ".md"
+    ],
+    "Archives": [
+        ".zip",
+        ".tar"
+    ],
+    "Installers": [
+        ".dmg",
+        ".pkg"
+    ],
+    "Videos": [
+        ".mp4",
+        ".mov",
+        ".avi",
+        ".mkv",
+        ".webm"
+    ],
+    "Audio": [
+        '.mp3', 
+        '.wav', 
+        '.m4a', 
+        '.flac', 
+        '.aac'    
+    ],
+    "Code": [
+        ".py",
+        ".js",
+        ".html",
+        ".css",
+        ".java",
+        ".c",
+        ".cpp",
+        ".cs",
+        ".rb",
+        ".php",
+        ".go",
+        ".rs",
+        ".ts",
+        ".swift",
+        ".kt",
+        ".m",
+        ".sh",
+        ".bat",
+        ".pl",
+        ".r",
+        ".lua",
+        ".sql",
+        ".xml",
+        ".json",
+        ".yaml",
+        ".yml",
+        ".ini",
+        ".md",
+    ],
+    "Libraries/Binaries": [
+        ".jar", 
+        ".node", 
+        ".dylib", 
+        ".dll", 
+        ".so", 
+        ".a", 
+        ".lib", 
+        ".wasm"
+    ],
+    "System Files": [
+        ".sys",
+        ".plist",
+        ".log",
+        ".nib",
+        ".DS_Store",
+        ".localized",
+        ".map"
+    ]
+}
+
+def scan_folder(folder_path):
+    dir = Path(folder_path).expanduser()
+
+    total_size = 0
+    num_files = 0
+
+    images_size = 0
+    documents_size = 0
+    archives_size = 0
+    installers_size = 0
+    videos_size = 0
+    audio_size = 0
+    code_size = 0
+    libin_size = 0
+    sysfiles_size = 0
+    others_size = 0
+
+    # for testing
+    others_size = 0
+
+    # get data about all files in the folder/subfolders
+    for file in dir.rglob('*'):
+        if file.is_file():
+            info = file.stat()
+            size = info.st_size
+            created_time = datetime.fromtimestamp(info.st_ctime)
+            modified_time = datetime.fromtimestamp(info.st_mtime)
+
+            file_category = [key for key, val in category.items() if file.suffix.lower() in val]
+            total_size += size
+            num_files += 1
+
+            if file_category:
+                if file_category[0] == "Images":
+                    images_size += size
+                elif file_category[0] == "Documents":
+                    documents_size += size
+                elif file_category[0] == "Archives":
+                    archives_size += size
+                elif file_category[0] == "Installers":
+                    installers_size += size
+                elif file_category[0] == "Videos or Music":
+                    videos_size += size
+                elif file_category[0] == "Code":
+                    code_size += size
+                elif file_category[0] == "Audio":
+                    audio_size += size
+                elif file_category[0] == "Libaries/Binaries":
+                    libin_size += size
+                elif file_category[0] == "System Files":
+                    sysfiles_size += size
+            else:
+                others_size += size
+
+    total_size /= 1000000000 # convert to GB
+    total_size = round(total_size, 2)
+
+    # printing the results
+
+    print(f"Total size: {total_size}GB")
+    print(f"Number of files: {num_files}")
+
+    print(f"Images size: {round(images_size / 1000000000, 2)}GB")
+    print(f"Documents size: {round(documents_size / 1000000000, 2)}GB")
+    print(f"Archives size: {round(archives_size / 1000000000, 2)}GB")
+    print(f"Installers size: {round(installers_size / 1000000000, 2)}GB")
+    print(f"Videos size: {round(videos_size / 1000000000, 2)}GB")
+    print(f"Audio size: {round(audio_size / 1000000000, 2)}GB")
+    print(f"Code size: {round(code_size / 1000000000, 2)}GB")
+    print(f"Libraries/Binaries size: {round(libin_size / 1000000000, 2)}GB")
+    print(f"System Files size: {round(sysfiles_size / 1000000000, 2)}GB")
+    print(f"Others size: {round(others_size / 1000000000, 2)}GB")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python scanner.py <folder_path>")
+    else:
+        scan_folder(sys.argv[1])
